d985cb21fbacecbcddbbc80ffe59c7c299326d30
Blake Mesdag <blakemesdag@gmail.com>
2015-11-23 11:04:23 -0500

    Add some barebones stuff

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,13 @@
 class OrdersController < ApplicationController
   def index
+    @orders = Order.all
   end
 
   def show
+    @order = Order.find(params[:id])
   end
 
   def search
+    @orders = []
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,55 @@
 
 def create_order
   order = Order.new
+  order.customer = Customer.new
+
+  order.email = order.customer.email = Faker::Internet.email
+
   order.shipping_address = Address.new
   order.billing_address = Address.new
   order.line_items = [LineItem.new, LineItem.new, LineItem.new]
+
+  populate_address(order.shipping_address)
+
+  if rand(10) > 5
+    order.billing_address.attributes = order.shipping_address.attributes
+  else
+    populate_address(order.billing_address)
+  end
+
+  populate_order_line_items(order)
+
   order.save!
 end
 
-1.times do
+def populate_order(order)
+  order.notes = Faker::Lorem.paragraph
+
+  order
+end
+
+def populate_address(address)
+  name = Faker::Name.name
+
+  address.first_name, address.last_name = *name.split(' ')
+  address.address1 = Faker::Address.street_address
+  address.country = Faker::Address.country
+  address.state = Faker::Address.state
+  address.city = Faker::Address.city
+  address.postal_code = Faker::Address.zip
+  address.phone_number = Faker::PhoneNumber.phone_number
+
+  address
+end
+
+def populate_order_line_items(order)
+  order.line_items.each do |line_item|
+    line_item.title = Faker::Commerce.product_name
+    line_item.price = Faker::Commerce.price
+  end
+end
+
+
+10_000.times do
   create_order
 end
